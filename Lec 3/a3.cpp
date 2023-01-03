2992cf1b24049480ba603b121bcec7cd7ff4cc11
fayez-anwar <121453953+fayez-anwar@users.noreply.github.com>
2023-01-03 16:33:28 +0530

    Update a3.cpp

--- Lec 3/a3.cpp
+++ Lec 3/a3.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+using namespace std;
+int main() {
+    //take input n
+    int n;
+    cin>>n;
 
+    int sum = 0;
+    int maxValue = -9999999, minValue = 9999999;
+    for(int i = 1; i <= n; i++){
+        int x;
+        cin>>x;
+        
+        //can x be maximum
+        if(x > maxValue){
+            //then x is the maximum value
+            maxValue = x;
+        }
+        
+        //can x be minimum
+        if(x < minValue){
+            minValue = x;
+        }
+        sum += x;
+    }
+    cout<<"Sum: "<<sum<<endl;
+    float mean = (float)sum/n;
+    cout<<"Mean: "<<mean<<endl;
+
+    cout<<"Minimum Value: "<<minValue<<endl;
+    cout<<"Maximum Value: "<<maxValue<<endl;
+}
